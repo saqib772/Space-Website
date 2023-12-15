84088b7a7ca847dd9e77cb02c532b6a6c8e58b33
saqib iqbal <saqiboiqbal7772@gmail.com>
2023-12-15 20:26:58 +0500

    slider js updated

--- assets/js/image-slider.js
+++ assets/js/image-slider.js
@@ -1,52 +1,104 @@
-var slides = document.querySelectorAll(".slide");
-var btns = document.querySelectorAll(".slide-nav-btn");
+// var slides = document.querySelectorAll(".slide");
+// var btns = document.querySelectorAll(".slide-nav-btn");
+// let currentSlide = 1;
+
+// // JS for MANUAL NAVIGATION
+// var manualNav = function (manual) {
+//   slides.forEach((slide) => {
+//     slide.classList.remove("active");
+
+//     btns.forEach((btn) => {
+//       btn.classList.remove("active");
+//     });
+//   });
+
+//   slides[manual].classList.add("active");
+//   btns[manual].classList.add("active");
+// };
+
+// btns.forEach((btn, i) => {
+//   btn.addEventListener("click", () => {
+//     manualNav(i);
+//     currentSlide = i;
+//   });
+// });
+
+// // JS for AUTOPLAY
+// var repeat = function (activeClass) {
+//   let active = document.getElementsByClassName("active");
+//   let i = 1;
+
+//   var repeater = () => {
+//     setTimeout(function () {
+//       [...active].forEach((activeSlide) => {
+//         activeSlide.classList.remove("active");
+//       });
+
+//       slides[i].classList.add("active");
+//       btns[i].classList.add("active");
+//       i++;
+
+//       if (slides.length == i) {
+//         i = 0;
+//       }
+//       if (i >= slides.length) {
+//         return;
+//       }
+//       repeater();
+//     }, 1000);
+//   };
+//   repeater();
+// };
+// repeat();
+
+const slides = document.querySelectorAll(".slide");
+const btns = document.querySelectorAll(".slide-nav-btn");
 let currentSlide = 1;
 
-// JS for MANUAL NAVIGATION
-var manualNav = function (manual) {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
+// Improved `manualNav` function with synchronization check
+function manualNav(manualSlideIndex) {
+  if (manualSlideIndex === currentSlide) return; // Skip update if already active
 
-    btns.forEach((btn) => {
-      btn.classList.remove("active");
-    });
-  });
+  slides.forEach(slide => slide.classList.remove("active"));
+  btns.forEach(btn => btn.classList.remove("active"));
 
-  slides[manual].classList.add("active");
-  btns[manual].classList.add("active");
-};
+  slides[manualSlideIndex].classList.add("active");
+  btns[manualSlideIndex].classList.add("active");
+  currentSlide = manualSlideIndex;
+}
 
-btns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    manualNav(i);
-    currentSlide = i;
-  });
-});
+// Optimized `repeat` function using `setInterval`
+function repeat() {
+  const active = document.getElementsByClassName("active");
 
-// JS for AUTOPLAY
-var repeat = function (activeClass) {
-  let active = document.getElementsByClassName("active");
-  let i = 1;
+  let currentSlideIndex = currentSlide;
+  const slideNext = function () {
+    slides[currentSlideIndex].classList.remove("active");
+    btns[currentSlideIndex].classList.remove("active");
 
-  var repeater = () => {
-    setTimeout(function () {
-      [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove("active");
-      });
+    currentSlideIndex++;
+    if (currentSlideIndex >= slides.length) {
+      currentSlideIndex = 0;
+    }
 
-      slides[i].classList.add("active");
-      btns[i].classList.add("active");
-      i++;
-
-      if (slides.length == i) {
-        i = 0;
-      }
-      if (i >= slides.length) {
-        return;
-      }
-      repeater();
-    }, 10000);
+    slides[currentSlideIndex].classList.add("active");
+    btns[currentSlideIndex].classList.add("active");
   };
-  repeater();
-};
+
+  const interval = setInterval(slideNext, 100); // Adjust interval as needed
+
+  // Stop autoplay on manual navigation or window resize
+  btns.forEach(btn => btn.addEventListener("click", () => clearInterval(interval)));
+  window.addEventListener("resize", () => clearInterval(interval));
+}
+
+// Initialize autoplay
 repeat();
+
+// Bind click events to navigation buttons
+btns.forEach((btn, i) => btn.addEventListener("click", () => manualNav(i)));
+
+// Optionally implement animation using CSS transitions or JavaScript libraries
+
+// Consider adding error handling for missing elements or invalid class names
+
